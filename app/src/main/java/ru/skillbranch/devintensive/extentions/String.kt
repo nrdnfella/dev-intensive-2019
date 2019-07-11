af0b361b14731a7f99b4eb895a0ca180894f323b
Andrey Maykut <maykout@yandex.ru>
2019-07-11 20:51:07 +0300

    Add stripHtml method

--- app/src/main/java/ru/skillbranch/devintensive/extentions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extentions/String.kt
@@ -4,3 +4,5 @@ fun String.truncate(value: Int = 16): String  {
     val truncString = substring(0..if(value<0) 0 else value-1).trimEnd();
     return if (truncString.length == value) truncString + "..." else truncString
 }
+
+fun String.stripHtml(): String = this.replace(Regex("(<(.|\\n)+?>)|(&quot;)|(&amp;)|(&gt;)|(&lt;)"), "").replace(Regex("\\s+"), " ")
